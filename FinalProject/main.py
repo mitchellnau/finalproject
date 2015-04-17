a9cc089986961b88dd61855100c2e29ed48fd717
SuitPotato <Erekyth@yahoo.com>
2015-04-16 21:36:05 -0700

    Main w/ Item Class

--- FinalProject/main.py
+++ FinalProject/main.py
@@ -19,7 +19,16 @@ from google.appengine.ext.webapp import template
 from google.appengine.api import users
 from google.appengine.ext import ndb
 
-
+class Item(ndb.Model):
+	name = ndb.StringProperty(required=True)
+	#Can use queries on type property
+	type = ndb.StringProperty()
+	description = ndb.TextProperty()
+	#Can change to integer if needed, can use Amazon or Paypal to handle money
+	price = ndb.FloatProperty()
+	stock = ndb.IntegerProperty()
+	#Potential Others: Reviews
+	
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
