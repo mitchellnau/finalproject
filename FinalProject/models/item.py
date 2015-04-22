d4acfcf2fc7515e66de7ba69564592fb66d3770d
mitchellnau <mmh327@nau.edu>
2015-04-21 20:51:51 -0700

    models into their own directory from main.py

---
+++ FinalProject/models/item.py
@@ -0,0 +1,11 @@
+from google.appengine.ext import ndb
+
+class Item(ndb.Model):
+	name = ndb.StringProperty(required=True)
+	#Can use queries on type property
+	type = ndb.StringProperty()
+	description = ndb.TextProperty()
+	#Can change to integer if needed, can use Amazon or Paypal to handle money
+	price = ndb.FloatProperty()
+	stock = ndb.IntegerProperty()
+	#Potential Others: Reviews
