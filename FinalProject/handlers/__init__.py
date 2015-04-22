1c3c13a276de8e6fb75b433b8c7ad254c433f186
mitchellnau <mmh327@nau.edu>
2015-04-21 21:05:59 -0700

    index handler

--- FinalProject/handlers/__init__.py
+++ FinalProject/handlers/__init__.py
@@ -1,2 +1,3 @@
 from base_handler import BaseHandler
 from splash_page import SplashPage
+from index_page import IndexPage
---
+++ FinalProject/handlers/index_page.py
@@ -0,0 +1,6 @@
+import webapp2
+from handlers import BaseHandler
+
+class IndexPage(BaseHandler):
+    def get(self):
+        self.render("index.html", {})
