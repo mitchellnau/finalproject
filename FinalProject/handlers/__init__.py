fc57800bdc7644914356925e1c7c1cf3643ea7fd
mitchellnau <mmh327@nau.edu>
2015-04-29 19:46:41 -0700

    renamed splash->login and logout imports

--- FinalProject/handlers/__init__.py
+++ FinalProject/handlers/__init__.py
@@ -1,5 +1,6 @@
 from base_handler import BaseHandler
-from splash_page import SplashPage
+from login_page import LoginPage
+from logout_page import LogoutPage
 from index_page import IndexPage
 from new_item import NewItem
 from item_input import ItemInput
