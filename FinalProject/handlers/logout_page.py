ed3f4a2a381ce311ce5def30c01856e39ed7bbb4
mitchellnau <mmh327@nau.edu>
2015-04-29 19:46:11 -0700

    added logout functionality

---
+++ FinalProject/handlers/logout_page.py
@@ -0,0 +1,15 @@
+import webapp2
+from google.appengine.ext.webapp import template
+from google.appengine.api import users
+from handlers import BaseHandler
+
+class LogoutPage(BaseHandler):
+    def get(self):
+        # Checks for active Google account session
+        user = users.get_current_user()
+
+        if user:
+            self.redirect(users.create_logout_url('/'))
+        else:
+            self.redirect('/login')
+
