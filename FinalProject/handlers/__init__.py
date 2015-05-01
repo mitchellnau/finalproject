b18716cff69336ce3f885aa0467ce03e30af7418
SuitPotato <Erekyth@yahoo.com>
2015-05-01 00:08:44 -0700

    init.py handler additions

--- FinalProject/handlers/__init__.py
+++ FinalProject/handlers/__init__.py
@@ -10,3 +10,6 @@ from loggedin_page import LoggedInPage
 from loggedout_page import LoggedOutPage
 from store_handler import StoreHandler
 from store_clothes import StoreClothes
+from store_vehicle import StoreVehicle
+from store_furniture import StoreFurniture
+from store_electronics import StoreElectronics
