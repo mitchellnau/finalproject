896b173a21545c8e1e3af910af8ec6e872de8a60
SuitPotato <Erekyth@yahoo.com>
2015-05-01 00:07:45 -0700

    store.vehicle.py

---
+++ FinalProject/handlers/store_vehicle.py
@@ -0,0 +1,14 @@
+import webapp2
+from google.appengine.api import users
+from handlers import BaseHandler
+from models import Item
+
+class StoreVehicle(BaseHandler):
+	def get(self):
+		user = users.get_current_user()
+		items = Item.query().filter(Item.type == 'Vehicles').order(-Item.date).fetch(10)
+
+		template_values = {
+			'items':items
+		}
+		self.render("store.html", (template_values))
