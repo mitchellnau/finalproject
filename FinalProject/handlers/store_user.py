31501d972c858409cbea72970a83d98e4169b1ed
SuitPotato <Erekyth@yahoo.com>
2015-05-01 01:08:32 -0700

    store_user.py

---
+++ FinalProject/handlers/store_user.py
@@ -0,0 +1,14 @@
+import webapp2
+from google.appengine.api import users
+from handlers import BaseHandler
+from models import Item
+
+class StoreUser(BaseHandler):
+	def get(self):
+		user = users.get_current_user()
+		items = Item.query().filter(Item.owner == user.email()).order(-Item.date).fetch(10)
+
+		template_values = {
+			'items':items
+		}
+		self.render("user_store.html", (template_values))
