574dc1faae5e69dbb8586a7ff076cef09f1de06e
SuitPotato <Erekyth@yahoo.com>
2015-05-01 00:07:31 -0700

    store_furniture

---
+++ FinalProject/handlers/store_furniture.py
@@ -0,0 +1,14 @@
+import webapp2
+from google.appengine.api import users
+from handlers import BaseHandler
+from models import Item
+
+class StoreFurniture(BaseHandler):
+	def get(self):
+		user = users.get_current_user()
+		items = Item.query().filter(Item.type == 'Furniture').order(-Item.date).fetch(10)
+
+		template_values = {
+			'items':items
+		}
+		self.render("store.html", (template_values))
