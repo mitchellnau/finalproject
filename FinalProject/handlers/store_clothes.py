b6e38c218d6763cacea1c63c61981d2492dc0a5b
SuitPotato <Erekyth@yahoo.com>
2015-05-01 00:07:16 -0700

    store_clothes.py user

--- FinalProject/handlers/store_clothes.py
+++ FinalProject/handlers/store_clothes.py
@@ -1,10 +1,11 @@
 import webapp2
-
+from google.appengine.api import users
 from handlers import BaseHandler
 from models import Item
 
 class StoreClothes(BaseHandler):
 	def get(self):
+		user = users.get_current_user()
 		items = Item.query().filter(Item.type == 'Clothing').order(-Item.date).fetch(10)
 
 		template_values = {
