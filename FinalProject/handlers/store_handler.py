12d6c0f53901b7f2ee72660dcb5515678a18b6cc
SuitPotato <Erekyth@yahoo.com>
2015-04-30 23:50:14 -0700

    store_handler moving stuff around

--- FinalProject/handlers/store_handler.py
+++ FinalProject/handlers/store_handler.py
@@ -7,13 +7,12 @@ from models import Item
 
 class StoreHandler(BaseHandler):
 	def get(self):
-		items = Item.query().order(-Item.date).fetch(10)
-
-                user = users.get_current_user()
+		user = users.get_current_user()
+		items = Item.query().order(-Item.date).fetch(10)     
 		
 		template_values = {
 			'items':items,
-                        'user': user
+             'user': user
 		}
 		self.render("store.html", (template_values))
 		
